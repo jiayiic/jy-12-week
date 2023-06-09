75554153ab1110f3feb25887e451b3060fb74f22
Jiayi Chen <jychen075@gmail.com>
2023-06-09 12:02:09 -0400

    simplified

---
+++ pigGame-master/yearUpdater.js
@@ -0,0 +1,5 @@
+/* For any Question write me an Email at: codeWithJad@gmail.com */
+
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
